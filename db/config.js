05435f9eb067b14a5f21a4cfb516dd011ff87b86
Jorge Malo <jorge.malo@getbuilt.com>
2022-02-27 16:33:44 -0500

    Adds .env.sample file

--- db/config.js
+++ db/config.js
@@ -4,4 +4,5 @@ module.exports = {
   mongodb: {
     connectTo: (database) => `mongodb+srv://jorelmaro:${env.DB_PASSWORD}@coder.3c0d1.mongodb.net/${database}?retryWrites=true&w=majority`,
   }
+  // Change here for your mongo atlas account's URI
 }
